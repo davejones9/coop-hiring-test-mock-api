5fa4196fd54ea3d599e002d70b74728cfea0464e
Fred Marecesche <fred@froddd.co.uk>
2018-01-05 22:02:31 +0000

    Allow CORS

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (request, response) => {
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+app.get('/', (request, response, next) => {
   let target = (Math.round(Math.random() * 10) * 500) + 5000
   let raised = Math.round(Math.random() * target * 100) / 100
   let data = {
